03bd986ef0b31d3fb32b09855b63754457b3ccde
Kyle <kyle.lauritzen@gmail.com>
2015-12-03 14:31:24 -0700

    testing node endpoints with postman, get/post listings are working.

---
+++ server/controllers/listingCtrl.js
@@ -0,0 +1,26 @@
+var listings = require('../models/Listing');
+
+module.exports = {
+    
+    getListings: function(req,res,next) {
+        var newArr = [];
+        if (req.query.postNumber === undefined)
+            newArr = listings;
+        else {
+            
+             for (var i = 0; i < listings.length; i++) {
+                    console.log(req.query.postNumber);
+                    if (listings[i].postNumber == req.query.postNumber)
+                        newArr.push(listings[i]);
+                }
+        }
+        res.status(200).json(newArr);
+    },
+    
+    
+    
+    postNewListing: function(req,res,next) {
+        listings.push(req.body);
+        res.status(200).json(listings);
+    },
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
+////////////////
+//DEPENDENCIES//
+////////////////
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 
-
+///////////////
+//CONTROLLERS//
+///////////////
+var listingCtrl = require('./controllers/listingCtrl');
+var userCtrl = require('./controllers/userCtrl');
 
 var app = express();
 
+//////////////
+//MIDDLEWARE//
+//////////////
 app.use(bodyParser.json());
 
+/////////////
+//ENDPOINTS//
+/////////////
+
+app.get('/api/listings', listingCtrl.getListings);
+
+app.post('/api/listings', listingCtrl.postNewListing);
+
+/*app.post('/api/account', userCtrl.createNewAccount);
+app.post('api/account', userCtrl.getUserAccount);*/
 
 
 
@@ -17,10 +37,9 @@ app.use(bodyParser.json());
 
 
 
-
-
-
-
+///////////////
+//CONNECTIONS//
+///////////////
 var port = 3000;
 app.listen(port, function() {
     
